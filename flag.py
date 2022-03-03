fb7004fd4c50842696b19a7de9c723a4cbe1a00e
khanhpc <95212482+khanhpc@users.noreply.github.com>
2022-03-03 09:17:09 +0700

    Update flag.py

--- flag.py
+++ flag.py
@@ -1,27 +1,33 @@
 import turtle
-vin = turtle.Turtle()
-wn = turtle.Screen()          
-wn.bgcolor("red")             
+p = turtle.Turtle()
+p.color("yellow")
+p.penup()
+sc = turtle.Screen()          
+sc.bgcolor("red")             
 screen_width = 600;           
 screen_height = 350;          
 star_length = 200;            
  
-wn.setup(width = screen_width, height = screen_height)
-wn.title("Flag of Vietnam")
-                # Set the color of the turtle to red
-# Move the turtle to a predefined coordinate
-vin.color("yellow")
-vin.penup()
-vin.setx((screen_width - star_length)/2 - screen_width/2)
-vin.sety(star_length/4)
-vin.color("yellow")
-vin.shape("blank")             
-vin.pensize(3)                 
- 
-vin.begin_fill()
+sc.setup(width = screen_width, height = screen_height)
+sc.title("Flag of Vietnam")
+
+p.setx((screen_width - star_length)/2 - screen_width/2)
+p.sety(star_length/4)
+p.shape("blank")             
+p.pensize(3)                 
+p.begin_fill()
+
 # Draw the flag
 for counter in range(5):
-    vin.forward(star_length)                 
-    vin.right(144)
-vin.end_fill()
+    p.forward(star_length)                 
+    p.right(144)
+p.end_fill()
+p.penup()
+p.hideturtle()
+p.penup()
+p.goto(0,-150)
+p.pendown()
+p.write("VietNam's National Flag", False, align="center",font=("Arial",30,"normal"))
+turtle.done()
+
  
